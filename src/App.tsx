f1656667d69b8d550cb0204b4251f81767264cfd
Karan Ghadi <karan10071@gmail.com>
2023-08-02 00:05:18 +0530

    added mobile animation

--- src/App.tsx
+++ src/App.tsx
@@ -95,9 +95,9 @@ function App() {
 				duration: 0.3
 			}
 			);
-	}, [])
+	}, []);
 
-	useEffect(() => {
+	const tweenAnimation: any = () =>{
 		if (window.innerWidth > 767) {
 			const element: any = ref.current;
 
@@ -177,8 +177,8 @@ function App() {
 
 
 			// Section Three Animations
-			gsap.to(".section_three_text", {
-				yPercent: 40,
+			gsap.to("#invisible-text", {
+				opacity: 0,
 				ease: "none",
 				scrollTrigger: {
 					trigger: element.querySelector(".section_three_text"),
@@ -188,7 +188,7 @@ function App() {
 				},
 			});
 			gsap.to(".section_three-top-right", {
-				yPercent: 60,
+				yPercent: 100,
 				ease: "none",
 				scrollTrigger: {
 					trigger: element.querySelector("#section_three"),
@@ -334,7 +334,131 @@ function App() {
 				},
 			});
 		}
+		if(window.innerWidth < 768){
+			const element: any = ref.current;
 
+			// Section One Animations
+			gsap.to(".first-paragraph", {
+				yPercent: 50,
+				opacity: 0,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector("#section_one"),
+					start: "top top", // the default values
+					// end: "bottom top",
+					scrub: true,
+				},
+			});
+
+			// Section Two Animations
+			gsap.to(".section_two-top-left", {
+				yPercent: 20,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector("#section_two"),
+					start: "top bottom", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			// Section Three Animations
+			gsap.to("#invisible-text", {
+				opacity: 0,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_three_text"),
+					start: "top center", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_three-top-right", {
+				yPercent: 30,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector("#section_three"),
+					start: "top bottom", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			// Section Four Animations
+			gsap.to(".section_four_center", {
+				xPercent: -10,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_four_center"),
+					start: "top center", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_four_side", {
+				xPercent: 10,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_four_side"),
+					start: "top center", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_four_side_b", {
+				xPercent: 10,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_four_side_b"),
+					start: "top center", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			// Section Five Animations
+			gsap.to(".section_five-highEndRetreatsParallax", {
+				yPercent: 70,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_five-highEndRetreatsParallax"),
+					start: "top center", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_five-companyOffsitesParallax", {
+				yPercent: -90,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_five-companyOffsitesParallax"),
+					start: "top bottom", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_five-uniqueCorporateEventsBlock", {
+				yPercent: -20,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_five-uniqueCorporateEventsBlock"),
+					start: "top bottom", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+			gsap.to(".section_five-sportMusicExperienceBlock", {
+				yPercent: -20,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element.querySelector(".section_five-sportMusicExperienceBlock"),
+					start: "top bottom", // the default values
+					// end: "bottom top",
+					scrub: true
+				},
+			});
+		}
+	}
+
+	useEffect(() => {
+		tweenAnimation();
 	}, []);
 
 
@@ -344,12 +468,10 @@ function App() {
 			<NavbarComponent />
 
 			{/* Section One */}
-			<div id="section_one" className='parralax flex items-center justify-center h-screen
-        xl:h-[62.75rem] w-full relative px-5 xl:px-0'>
+			<div id="section_one" className='parralax flex items-center justify-center h-screen xl:h-[62.75rem] w-full relative px-5 xl:px-0'>
 				<img src={topLeftRing} className='w-[40%] xl:w-[35%] absolute top-1/2 xl:top-0 left-0 -translate-y-1/2 xl:translate-y-0 ring-left' alt="top-section-rings" />
 				<img src={topRightRing} className='w-[40%] xl:w-[35%] absolute top-1/2 xl:top-0 right-0 -translate-y-1/2 xl:translate-y-0 ring-right' alt="top-section-rings" />
 				<div className='text-center first-paragraph overflow-hidden'>
-					{/* <Fade fraction={1} delay={200}> */}
 					<HeadingOneComponent className="justify-center">
 					<div className='inline-block text-b transition-all'>B</div><div className='inline-block text-e'>E</div><div className='hidden text-i'>I</div><div className='hidden text-z'>Z</div>
 						SPOKE
@@ -357,13 +479,12 @@ function App() {
 					<HeadingOneComponent>
 						<span className='text-[3rem] xl:text-[7rem] block'>EVENTS</span>
 					</HeadingOneComponent>
-					{/* </Fade> */}
 				</div>
 			</div>
 
 			{/* @section 2 */}
 			<div id='section_two' className='relative z-10 px-5 xl:px-0'>
-				<div className='py-28'>
+				<div className='py-20 xl:py-36'>
 					<h3 className='section-two-head-text font-secondary_Variable font-normal text-base xl:text-[2.5rem]
             text-[#707070] w-full xl:w-[63rem] mx-auto text-center leading-tight'>
 						Imagine a Team of Industry Veterans Tirelessly Curating Your Vision, Personalised and Evolving With Your Needs.
@@ -375,39 +496,39 @@ function App() {
 						</AnchorLinksComponents>
 					</div>
 				</div>
-				<div className='section_two-top-left absolute -bottom-72 left-10'>
+				<div className='section_two-top-left absolute -bottom-60 left-10 hidden xl:block w-[30rem]'>
 					<img src={sectionThreeLeft} alt='side small' />
 				</div>
 			</div>
 
 			{/* @Section 3 */}
 
-			<div id='section_three' className='parralax h-screen w-full flex text-center items-center
-        justify-center relative z-20'>
-
-				<div className='section_three-top-right absolute -top-16 right-10 z-10'>
+			<div id='section_three' className='parralax h-screen w-full flex text-center items-end xl:items-center justify-center relative z-20'>
+				<div className='section_two-top-left absolute top-28 left-9 xl:left-10 block xl:hidden w-[70%] xl:w-auto'>
+					<img src={sectionThreeLeft} alt='side small' />
+				</div>
+				<div className='section_three-top-right absolute bottom-48 xl:bottom-auto xl:-top-16 right-9 xl:right-10 z-10 w-[70%] xl:w-[25rem]'>
 					<img src={sectionThreeRight} alt='side small' />
 				</div>
-				<div className='section_three-center-bottom absolute -bottom-16 left-0 right-0 grid place-items-center z-10'>
+				<div className='hidden xl:grid section_three-center-bottom absolute -bottom-16 left-0 right-0 place-items-center z-10 w-[25rem] xl:mx-auto'>
 					<img src={sectionTwoBottom} alt='side small' />
 				</div>
 
 				<div className='section_three_text relative z-30'>
 					<h4 className='font-secondary_Variable font-normal text-[1.5rem] xl:text-[3.5rem] text-white
             w-full xl:w-[48rem] mx-auto text-center leading-tight'>
-						Focus on the bigger picture, <span className='block'>while we make the work invisible</span>
+						Focus on the bigger picture, <span className='block'>while we make the work <span id="invisible-text">invisible</span></span>
 					</h4>
 					<AnchorLinksComponents class="flex hover:underline justify-center">
-						Explore Possibilities <img src={arrowRight} alt='arrow' className='ml-2' />
+						Explore Possibilities <img src={arrowRight} alt='arrow' className='ml-2 w-[1rem] xl:w-[2rem]' />
 					</AnchorLinksComponents>
 				</div>
 			</div>
 
 			{/* @Section 4 */}
 			<div id='section_four' className='w-full text-center'>
-				<div className="section_four-title w-full xl:w-[60rem] mx-auto py-10 xl:py-24">
-					<h4 className='font-secondary_Variable font-normal text-[2rem] xl:text-[6rem] text-white w-full
-            xl:w-[48rem] mx-auto text-center leading-tight'>
+				<div className="section_four-title w-full xl:w-[60rem] mx-auto py-10 xl:py-24 mt-16 mb-8">
+					<h4 className='font-secondary_Variable font-normal text-[2rem] xl:text-[6rem] text-white w-full xl:w-[48rem] mx-auto text-center leading-tight mb-3'>
 						Relationships <span className='text-white block'>that give us an edge</span>
 					</h4>
 					<p className='font-primary_Variable font-normal text-base xl:text-[2rem] text-gray mx-auto text-center leading-tight'>
@@ -417,81 +538,80 @@ function App() {
 				</div>
 				<div className='block xl:flex items-start justify-between w-full xl:w-[65rem] mx-auto pb-24'>
 					<div className='section_four_side'>
-						<img src={sectionFourImageOne} alt='sectionEdgeImgA' className='mb-14' />
-						<img src={sectionFourImageTwo} alt='sectionEdgeImgA' className='mb-14' />
+						<img src={sectionFourImageOne} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
+						<img src={sectionFourImageTwo} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
 					</div>
 					<div className='section_four_center xl:mt-28'>
-						<img src={sectionFourImageThree} alt='sectionEdgeImgA' className='mb-14' />
-						<img src={sectionFourImageFour} alt='sectionEdgeImgA' className='mb-14' />
+						<img src={sectionFourImageThree} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
+						<img src={sectionFourImageFour} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
 					</div>
 					<div className='section_four_side_b'>
-						<img src={sectionFourImageFive} alt='sectionEdgeImgA' className='mb-14' />
-						<img src={sectionFourImageSix} alt='sectionEdgeImgA' className='mb-14' />
+						<img src={sectionFourImageFive} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
+						<img src={sectionFourImageSix} alt='sectionEdgeImgA' className='mb-14 mx-auto' />
 					</div>
 				</div>
 			</div>
 
 			{/* @section 5 */}
-			<div id="section_five" className='font-secondary_Variable font-normal text-[6rem]
-        text-white mx-auto text-center leading-tight xl:py-9'>
+			<div id="section_five" className='font-secondary_Variable font-normal text-[6rem] text-white mx-auto text-center leading-tight xl:py-9'>
 				<div className='section_five-title w-full xl:w-[56rem] mx-auto'>
 					<h4 className='text-[2rem] xl:text-[4.5rem] text-[#E4E4E4] font-secondary_Variable font-bold'>
 						Bring Your Vision Alive, Afterall The Possibilities
 						<span className='italic font-medium block'>Are Endless</span>
 					</h4>
 				</div>
-				<div className='mt-60 px-5 xl:px-0'>
+				<div className='mt-32 xl:mt-60 px-5 xl:px-0 xl:w-[80rem] xl:mx-auto'>
 					<div className='w-full xl:w-[80rem] relative mx-auto block xl:flex justify-start items-center'>
-						<div className='section_five-highEndRetreatsParallax absolute xl:-top-6 xl:-left-9 z-20'>
+						<div className='section_five-highEndRetreatsParallax absolute -top-4 xl:-top-6 -left-2 xl:-left-9 z-20 w-[10rem] xl:w-auto'>
 							<img src={highEndRetreatsParallax} alt='side small' />
 						</div>
-						<div className='section_five-highEndRetreats img-section'>
+						<div className='section_five-highEndRetreats img-section xl:w-[42rem]'>
 							<img src={highEndRetreats} alt="side" />
 						</div>
-						<div className='content-section w-full xl:w-[30rem] ml-10 text-left'>
+						<div className='content-section w-full xl:w-[30rem] xl:ml-10 mt-5 xl:mt-0 text-left'>
 							<HeadingFourComponent>
 								<span className='block xl:ml-28'>High End</span> Retreats
 							</HeadingFourComponent>
 							<SubHeadingOneComponent>
-								<span className='block xl:ml-20 mt-8'>
+								<span className='block xl:ml-20 mt-4 xl:mt-8'>
 									Network In The Lap Of Luxury With Business Leaders, Friends & Family
 								</span>
 							</SubHeadingOneComponent>
 						</div>
 					</div>
 				</div>
-				<div className='mt-60'>
-					<div className='w-full xl:w-[80rem] relative mx-auto block xl:flex justify-start items-center'>
-						<div className='content-section w-full xl:w-[30rem] mr-10 text-left'>
-							<HeadingFourComponent className="text-right xl:-mr-10 relative z-10">
+				<div className='mt-32 xl:mt-60 px-5 xl:px-0 xl:w-[80rem] xl:mx-auto'>
+					<div className='w-full xl:w-[80rem] relative mx-auto flex flex-col xl:flex-row justify-start items-center'>
+						<div className='order-2 xl:order-1 content-section w-full xl:w-[30rem] xl:mr-10 text-left mt-5 xl:mt-0'>
+							<HeadingFourComponent className="text-left xl:text-right xl:-mr-10 relative z-10">
 								COMPANY<span className='block xl:-mr-10'>OFFSITES</span>
 							</HeadingFourComponent>
 							<SubHeadingOneComponent>
-								<span className='block xl:mt-8'>
+								<span className='block mt-4 xl:mt-8'>
 									Recharge And Inspire Teams As You Continue To Scale Your Business
 								</span>
 							</SubHeadingOneComponent>
 						</div>
-						<div className='section_five-companyOffsitesParallax absolute xl:-bottom-2 xl:-right-2 z-10'>
-							<img src={companyOffsitesParallax} alt='side small' />
-						</div>
-						<div className='section_five-companyOffsites img-section'>
+						<div className='section_five-companyOffsites img-section order-1 xl:order-2 xl:w-[50rem]'>
 							<img src={companyOffsitesBlock} alt="side" />
+						</div>
+						<div className='section_five-companyOffsitesParallax absolute bottom-40 right-4 xl:-right-2 z-10 w-[10rem] xl:w-auto'>
+							<img src={companyOffsitesParallax} alt='side small'/>
 						</div>
 					</div>
 				</div>
-				<div className='mt-60'>
-					<div className='w-full xl:w-[80rem] relative mx-auto block xl:flex justify-start items-center'>
-						<div className='section_five-uniqueCorporateEventsBlock img-section'>
+				<div className='mt-32 xl:mt-60 px-5 xl:px-0 xl:w-[80rem] xl:mx-auto'>
+					<div className='w-full xl:w-[80rem] relative mx-auto flex flex-col xl:flex-row justify-start xl:items-center'>
+						<div className='section_five-uniqueCorporateEventsBlock img-section xl:w-[50rem]'>
 							<img src={uniqueCorporateEventsBlock} alt="side" />
 						</div>
-						<div className='content-section xl:w-[30rem] xl:ml-10 text-left relative z-10'>
+						<div className='content-section xl:w-[30rem] xl:ml-10 text-left relative z-10 mt-5 xl:mt-0'>
 							<HeadingFourComponent className="leading-[1]">UNIQUE
 								<span className='block xl:-ml-28'>CORPORATE</span>
 								<span className='block'>EVENTS</span>
 							</HeadingFourComponent>
 							<SubHeadingOneComponent>
-								<span className='block xl:ml-20 mt-8 text-right leading-8'>
+								<span className='block xl:ml-20 mt-4 xl:mt-8 text-left xl:text-right leading-5 xl:leading-8'>
 									For Teams, Clients Or Specific
 									<span className='block'>Audiences - You Envision It,</span>
 									<span className='block'>We’ll Build It</span>
@@ -500,19 +620,19 @@ function App() {
 						</div>
 					</div>
 				</div>
-				<div className='my-60'>
-					<div className='w-full xl:w-[84rem] relative mx-auto block xl:flex justify-start items-center'>
-						<div className='content-section xl:w-[30rem] text-left'>
-							<HeadingFourComponent className="text-right relative z-10 xl:-mr-10">
+				<div className='mt-32 xl:mt-60 mb-60 px-5 xl:px-0 xl:w-[80rem] xl:mx-auto'>
+					<div className='w-full xl:w-[84rem] relative mx-auto flex flex-col xl:flex-row justify-start items-center'>
+						<div className='content-section xl:w-[30rem] text-left order-2 xl:order-1 mt-5 xl:mt-0'>
+							<HeadingFourComponent className="text-left xl:text-right relative z-10 xl:-mr-10">
 								SPORT & MUSIC <span className='block'>EXPERIENCES</span>
 							</HeadingFourComponent>
 							<SubHeadingOneComponent>
-								<span className='block mt-8'>
+								<span className='block mt-4 xl:mt-8'>
 									Enjoy All-Access At Some Of The Most Sought After Global Events.
 								</span>
 							</SubHeadingOneComponent>
 						</div>
-						<div className='section_five-sportMusicExperienceBlock img-section'>
+						<div className='section_five-sportMusicExperienceBlock img-section order:1 xl:order-2 xl:w-[50rem]'>
 							<img src={sportMusicExperienceBlock} alt="side" />
 						</div>
 					</div>
--- src/components/navbar/NavBarComponent.tsx
+++ src/components/navbar/NavBarComponent.tsx
@@ -1,21 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from './../../assets/images/svg/logo.svg'
 
+const navigation = [
+	{ name: 'About Us', href: '/' },
+	{ name: 'Products', href: '/' },
+	{ name: 'Build Your Vision', href: '/' }
+]
+
 export const NavbarComponent = () => {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 	return (
 		<>
-			<nav className="w-full py-4 px-8 justify-between items-center gap-96 hidden xl:inline-flex fixed top-0 left-0 bg-black z-50">
-				<div className="xl:w-96">
-					<a href="/">
-						<img src={logo} alt="Bizspoke Logo" />
-					</a>
-				</div>
-				<ul className="flex justify-end items-center gap-6">
-					<li className="font-primary_Medium text-2xl text-white">About Us</li>
-					<li className="font-primary_Medium text-2xl text-white">Project</li>
-					<li className="font-primary_Medium text-2xl text-white">Build Your Vision</li>
-				</ul>
-			</nav>
+			<header className="bg-black fixed top-0 left-0 z-30 w-full">
+				<nav className="mx-auto flex w-full items-center justify-between p-6 lg:px" aria-label="Global">
+					<div className="flex lg:flex-1">
+						<a href="/" className="-m-1.5 p-1.5">
+							<span className="sr-only">Bizspoke</span>
+							<img className="h-8 w-auto" src={logo} alt="logo" />
+						</a>
+					</div>
+					<div className="flex lg:hidden text-white">
+						<button
+							type="button"
+							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+							onClick={() => setMobileMenuOpen(true)}
+						>
+							<span className="sr-only">Open main menu</span>
+							<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="hidden lg:flex lg:gap-x-12">
+						{navigation.map((item) => (
+							<a key={item.name} href={item.href} className="font-primary_Medium text-2xl text-white">
+								{item.name}
+							</a>
+						))}
+					</div>
+				</nav>
+				<Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+					<div className="fixed inset-0 z-10" />
+					<Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+						<div className="flex items-center justify-between">
+							<a href="/" className="-m-1.5 p-1.5">
+								<span className="sr-only">Bizspoke</span>
+								<img
+									className="h-8 w-auto"
+									src={logo}
+									alt=""
+								/>
+							</a>
+							<button
+								type="button"
+								className="-m-2.5 rounded-md p-2.5 text-gray-400"
+								onClick={() => setMobileMenuOpen(false)}
+							>
+								<span className="sr-only">Close menu</span>
+								<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+							</button>
+						</div>
+						<div className="mt-6 flow-root">
+							<div className="-my-6">
+								<div className="space-y-2 py-6">
+									{navigation.map((item) => (
+										<a
+											key={item.name}
+											href={item.href}
+											className="-mx-3 block rounded-lg px-3 py-2 font-primary_Medium text-2xl leading-7 text-white"
+										>
+											{item.name}
+										</a>
+									))}
+								</div>
+
+							</div>
+						</div>
+					</Dialog.Panel>
+				</Dialog>
+			</header>
 		</>
 	);
 };
